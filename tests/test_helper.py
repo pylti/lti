dc52be3da5337c57bdf2042f944a5d39697a0716
Anson MacKeracher <amackera@gmail.com>
2012-05-07 11:34:43 -0400

    Added some initial test cases of tool_provider.py

---
+++ tests/test_helper.py
@@ -0,0 +1,19 @@
+from tool_provider import ToolProvider
+
+def create_params():
+  return {
+            "lti_message_type": "basic-lti-launch-request",
+            "lti_version": "LTI-1p0",
+            "resource_link_id": "c28ddcf1b2b13c52757aed1fe9b2eb0a4e2710a3",
+            "lis_result_sourcedid": "261-154-728-17-784",
+            "lis_outcome_service_url":"http://localhost/lis_grade_passback",
+            "launch_presentation_return_url": "http://example.com/lti_return",
+            "custom_param1": "custom1",
+            "custom_param2": "custom2",
+            "ext_lti_message_type": "extension-lti-launch",
+            "roles": "Learner,Instructor,Observer"
+    }
+
+def create_test_tp():
+    params = create_params()
+    return ToolProvider('hi', 'oi', params)
---
+++ tests/tool_provider_tests.py
@@ -0,0 +1,38 @@
+import unittest
+from test_helper import create_test_tp, create_params
+
+class TestToolProvider(unittest.TestCase):
+    def setUp(self):
+        self.params = create_params()
+        self.tp = create_test_tp()
+
+    def test_outcome_service(self):
+        self.assertTrue(self.tp.is_outcome_service())
+        self.tp.lis_result_sourcedid = None
+        self.assertFalse(self.tp.is_outcome_service())
+
+    def test_return_url_with_messages(self):
+        self.assertEqual(self.tp.build_return_url(),
+                self.params['launch_presentation_return_url'])
+        self.tp.lti_errormsg = 'user error message'
+        self.tp.lti_errorlog = 'lms error log'
+        self.lti_msg = 'user message'
+        self.ti_log = 'lms message'
+        self.assertEqual(self.tp.build_return_url(),
+                self.params['launch_presenation_return_url'] +
+                '?lti_errormsg=user%20error%20message&lti_errorlog=lms%20error%20log&lti_msg=user%20message&lti_log=lms%20message')
+
+    def test_rules(self):
+        self.assertTrue(self.tp.is_student())
+        self.assertTrue(self.tp.is_instructor())
+        self.assertTrue(self.tp.has_role('Observer'))
+        self.assertFalse(self.tp.has_role('administrator'))
+
+    def test_username(self):
+        self.assertEqual(self.tp.username('guy'), 'guy')
+        self.tp.lis_person_name_full = 'full'
+        self.assertEqual(self.tp.username('guy'), 'full')
+        self.tp.lis_person_name_family = 'family'
+        self.assertEqual(self.tp.username('guy'), 'family')
+        self.tp.lis_person_name_given = 'given'
+        self.assertEqual(self.tp.username('guy'), 'given')
