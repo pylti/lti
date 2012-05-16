6a78ae78651dd32bf236e131d97928272c3a35b2
Anson MacKeracher <amackera@gmail.com>
2012-05-16 13:01:21 -0400

    Implements the testURLQueryParameters test

--- tests/tool_consumer_tests.py
+++ tests/tool_consumer_tests.py
@@ -7,3 +7,12 @@ class TestToolConsumer(unittest.TestCase):
         result = tc.generate_launch_data()
         self.assertEqual(result['oauth_signature'],
                 'TPFPK4u3NwmtLt0nDMP1G1zG30U=')
+
+    def testURLQueryParameters(self):
+        tc = create_test_tc()
+        tc.launch_url = 'http://dr-chuck.com/ims/php-simple/tool.php?a=1&b=2&c=3%20%26a'
+        result = tc.generate_launch_data()
+        self.assetEquals(result['oauth_signature'], 
+                'uF7LooyefQN5aocx7UlYQ4tQM5k=')
+        self.assertEquals(result['c'], '3 &a')
+
