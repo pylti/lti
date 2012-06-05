1d14e1d06e519fe64f3a564711f008c7769f6108
Anson MacKeracher <amackera@gmail.com>
2012-06-05 18:41:34 -0400

    Adds stubs for OutcomeResponse tests

--- tests/outcome_response_test.py
+++ tests/outcome_response_test.py
@@ -1,1 +1,35 @@
-# TODO
+import unittest
+
+class TestOutcomeResponse(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_parse_replace_result_response_xml(self):
+        '''
+        Should parse replaceResult response XML.
+        '''
+        pass
+
+    def test_parse_read_result_response_xml(self):
+        '''
+        Should parse readResult response XML.
+        '''
+        pass
+
+    def test_parse_delete_result_response_xml(self):
+        '''
+        Should parse deleteResult response XML.
+        '''
+        pass
+
+    def test_recognize_failure_response(self):
+        '''
+        Should recognize a failure response.
+        '''
+        pass
+
+    def test_generate_response_xml(self):
+        '''
+        Should generate response XML.
+        '''
+        pass
