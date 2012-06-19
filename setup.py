49de7cf63c612d905462e5fc87d7caf8583a13c2
Anson MacKeracher <anson@tophatmonocle.com>
2012-06-19 18:48:40 -0400

    More setup.py stuff

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 from setuptools import setup, find_packages
 
-import os
+PKG = 'ims_lti_py'
 
-PKG = 'ims-lti-py'
-VERSIONFILE = os.path.join('ims-lti-py', '_version.py')
+setup(
+    name = PKG,
+    version = '0.1',
+    description = 'A Ruby library to help implement IMS LTI tool consumers and providers',
+    author = 'Anson MacKeracher',
+    author_email = 'anson@tophatmonocle.com',
+    url = 'https://github.com/tophatmonocle/ims-lti-py',
+    packages = find_packages(),
+    install_requires = ['lxml', 'oauth2'],
+    license = 'MIT License',
+    keywords = 'lti',
+    zip_save = True,
+    test_suite = 'tests'
+)
