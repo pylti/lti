4387aee2657d293e8816eaa300be5064c70bf9ca
Anson MacKeracher <anson@tophatmonocle.com>
2012-06-21 16:32:25 -0400

    Adds some classes to module interface

--- ims_lti_py/__init__.py
+++ ims_lti_py/__init__.py
@@ -1,3 +1,5 @@
 from tool_config import ToolConfig
 from tool_consumer import ToolConsumer
 from tool_provider import ToolProvider
+from outcome_request import OutcomeRequest
+from outcome_response import OutcomeResponse
