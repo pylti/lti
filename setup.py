8c4aad6fff41a7c4b9670d0fcc42917ad2765233
Anson MacKeracher <anson@tophatmonocle.com>
2013-02-01 11:39:16 -0500

    Increase version number to 0.6 and PEP8 setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'ims_lti_py',
-    version = '0.5',
-    description = 'A Python library to help implement IMS LTI tool consumers and providers',
-    author = 'Anson MacKeracher',
-    author_email = 'anson@tophatmonocle.com',
-    url = 'https://github.com/tophatmonocle/ims_lti_py',
-    packages = find_packages(),
-    install_requires = ['lxml', 'oauth2'],
-    license = 'MIT License',
-    keywords = 'lti',
-    zip_safe = True,
-    test_suite = 'tests',
+    name='ims_lti_py',
+    version='0.6',
+    description=('A Python library to help implement IMS '
+                 'LTI tool consumers and providers'),
+    author='Anson MacKeracher',
+    author_email='anson@tophatmonocle.com',
+    url='https://github.com/tophatmonocle/ims_lti_py',
+    packages=find_packages(),
+    install_requires=['lxml', 'oauth2'],
+    license='MIT License',
+    keywords='lti',
+    zip_safe=True,
+    test_suite='tests',
 )
