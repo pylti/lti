8dbbbfe2602774dc9304ac1a22108199cd5f144d
Michael Robellard <mike@noggingroup.com>
2017-05-11 10:11:50 -0400

    Added tests for LTI 2 proxy registration

--- src/lti/tool_proxy.py
+++ src/lti/tool_proxy.py
@@ -7,7 +7,7 @@ from requests_oauthlib.oauth1_auth import SIGNATURE_TYPE_AUTH_HEADER
 
 class ToolProxy(ToolBase):
     def load_tc_profile(self):
-        response = requests.get(self.launch_params['tc_profile_url'])
+        response = requests.get(self.tool_consumer_profile_url)
 
         self.tc_profile = json.loads(response.text)
 
@@ -20,10 +20,10 @@ class ToolProxy(ToolBase):
             if "application/vnd.ims.lti.v2.toolproxy+json" in service["format"] and "POST" in service["action"]:
                 return service["endpoint"]
 
-    def register_proxy(self, profile):
+    def register_proxy(self, tool_profile):
         register_url = self.find_registration_url()
 
-        r = Request("POST", register_url, data=json.dumps(profile, indent=4), headers={'Content-Type':'application/vnd.ims.lti.v2.toolproxy+json'}).prepare()
+        r = Request("POST", register_url, data=json.dumps(tool_profile, indent=4), headers={'Content-Type':'application/vnd.ims.lti.v2.toolproxy+json'}).prepare()
         sign = OAuth1(self.launch_params['reg_key'], self.launch_params['reg_password'],
                       signature_type=SIGNATURE_TYPE_AUTH_HEADER, force_include_body=True)
         signed = sign(r)
